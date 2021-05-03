82dd179a950c710dd144fce58922ebab7540ef8c
Daybrush <daybrush@gmail.com>
2021-05-03 16:34:00 +0900

    test: test

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,9 +1,14 @@
+// Set e2e config
 process.env.TS_NODE_PROJECT = "./test/e2e/tsconfig.json";
 require('ts-node/register')
+// Restore config
+process.env.TS_NODE_PROJECT = "";
+
 const { setHeadlessWhen } = require('@codeceptjs/configure');
+const execSh = require("exec-sh").promise;
+
 
 // turn on headless mode when running with HEADLESS=true environment variable
-// export HEADLESS=true && npx codeceptjs run
 setHeadlessWhen(process.env.HEADLESS);
 
 exports.config = {
@@ -20,12 +25,26 @@ exports.config = {
     },
     ResembleHelper: {
       require: "codeceptjs-resemblehelper",
-      screenshotFolder : "./test/e2e/log/output/",
+      screenshotFolder: "./test/e2e/log/output/",
       baseFolder: "./test/e2e/log/",
       diffFolder: "./test/e2e/log/diff/"
     }
   },
-  bootstrap: null,
+  async bootstrap() {
+    execSh(`npm run e2e:storybook`, {
+      cwd: __dirname,
+    }).catch(() => {
+      process.exit();
+    });
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, 30000);
+    });
+  },
+  async teardown() {
+    process.exit(0);
+  },
   mocha: {},
   name: 'egjs-flicking',
   plugins: {
